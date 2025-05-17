9f4bfdb6bb0f1450be59bb4427c276b49b4ae9c4
BercziSandor <BercziSandor@users.noreply.github.com>
2025-05-17 02:19:06 +0000

    Upgraded to CodeNarc 3.6.0-groovy-4.0

--- src/main/java/org/codenarc/idea/inspections/formatting/MissingBlankLineAfterPackageInspectionTool.java
+++ src/main/java/org/codenarc/idea/inspections/formatting/MissingBlankLineAfterPackageInspectionTool.java
@@ -1,31 +1,31 @@
 package org.codenarc.idea.inspections.formatting;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.psi.PsiElement;
-import java.util.Collection;
-import java.util.Collections;
-import javax.annotation.Generated;
-import org.codenarc.idea.CodeNarcInspectionTool;
-import org.codenarc.rule.Violation;
-import org.codenarc.rule.formatting.MissingBlankLineAfterPackageRule;
-import org.jetbrains.annotations.NotNull;
+        import com.intellij.codeInspection.LocalQuickFix;
+        import com.intellij.psi.PsiElement;
+        import java.util.Collection;
+        import java.util.Collections;
+        import javax.annotation.Generated;
+        import org.codenarc.idea.CodeNarcInspectionTool;
+        import org.codenarc.rule.Violation;
+        import org.codenarc.rule.formatting.MissingBlankLineAfterPackageRule;
+        import org.jetbrains.annotations.NotNull;
 
-@Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
-public class MissingBlankLineAfterPackageInspectionTool extends CodeNarcInspectionTool<MissingBlankLineAfterPackageRule> {
+        @Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
+        public class MissingBlankLineAfterPackageInspectionTool extends CodeNarcInspectionTool<MissingBlankLineAfterPackageRule> {
 
-    // this code has been generated from org.codenarc.rule.formatting.MissingBlankLineAfterPackageRule
+            // this code has been generated from org.codenarc.rule.formatting.MissingBlankLineAfterPackageRule
 
-    public static final String GROUP = "Formatting";
+            public static final String GROUP = "Formatting";
 
-    public MissingBlankLineAfterPackageInspectionTool() {
-        super(new MissingBlankLineAfterPackageRule());
-        applyDefaultConfiguration(getRule());
-    }
+            public MissingBlankLineAfterPackageInspectionTool() {
+                super(new MissingBlankLineAfterPackageRule());
+                applyDefaultConfiguration(getRule());
+            }
 
-    @Override
-    public String getRuleset() {
-        return GROUP;
-    }
+            @Override
+            public String getRuleset() {
+                return GROUP;
+            }
 
     // custom code can be written after this line and it will be preserved during the regeneration
 
--- src/main/java/org/codenarc/idea/inspections/size/NestedBlockDepthInspectionTool.java
+++ src/main/java/org/codenarc/idea/inspections/size/NestedBlockDepthInspectionTool.java
@@ -1,67 +1,67 @@
 package org.codenarc.idea.inspections.size;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.psi.PsiElement;
-import java.util.Collection;
-import java.util.Collections;
-import javax.annotation.Generated;
-import org.codenarc.idea.CodeNarcInspectionTool;
-import org.codenarc.rule.Violation;
-import org.codenarc.rule.size.NestedBlockDepthRule;
-import org.jetbrains.annotations.NotNull;
+        import com.intellij.codeInspection.LocalQuickFix;
+        import com.intellij.psi.PsiElement;
+        import java.util.Collection;
+        import java.util.Collections;
+        import javax.annotation.Generated;
+        import org.codenarc.idea.CodeNarcInspectionTool;
+        import org.codenarc.rule.Violation;
+        import org.codenarc.rule.size.NestedBlockDepthRule;
+        import org.jetbrains.annotations.NotNull;
 
-@Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
-public class NestedBlockDepthInspectionTool extends CodeNarcInspectionTool<NestedBlockDepthRule> {
+        @Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
+        public class NestedBlockDepthInspectionTool extends CodeNarcInspectionTool<NestedBlockDepthRule> {
 
-    // this code has been generated from org.codenarc.rule.size.NestedBlockDepthRule
+            // this code has been generated from org.codenarc.rule.size.NestedBlockDepthRule
 
-    public static final String GROUP = "Size";
+            public static final String GROUP = "Size";
 
-    public NestedBlockDepthInspectionTool() {
-        super(new NestedBlockDepthRule());
-        applyDefaultConfiguration(getRule());
-    }
+            public NestedBlockDepthInspectionTool() {
+                super(new NestedBlockDepthRule());
+                applyDefaultConfiguration(getRule());
+            }
 
-    @Override
-    public String getRuleset() {
-        return GROUP;
-    }
-
-
-    public void setApplyToClassNames(String value) {
-        getRule().setApplyToClassNames(value);
-    }
-
-    public String getApplyToClassNames() {
-        return getRule().getApplyToClassNames();
-    }
+            @Override
+            public String getRuleset() {
+                return GROUP;
+            }
 
 
-    public void setDoNotApplyToClassNames(String value) {
-        getRule().setDoNotApplyToClassNames(value);
-    }
+            public void setApplyToClassNames(String value) {
+                getRule().setApplyToClassNames(value);
+            }
 
-    public String getDoNotApplyToClassNames() {
-        return getRule().getDoNotApplyToClassNames();
-    }
-
-
-    public void setIgnoreRegex(String value) {
-        getRule().setIgnoreRegex(value);
-    }
-
-    public String getIgnoreRegex() {
-        return getRule().getIgnoreRegex();
-    }
+            public String getApplyToClassNames() {
+                return getRule().getApplyToClassNames();
+            }
 
 
-    public void setMaxNestedBlockDepth(int value) {
-        getRule().setMaxNestedBlockDepth(value);
-    }
+            public void setDoNotApplyToClassNames(String value) {
+                getRule().setDoNotApplyToClassNames(value);
+            }
 
-    public int getMaxNestedBlockDepth() {
-        return getRule().getMaxNestedBlockDepth();
-    }
+            public String getDoNotApplyToClassNames() {
+                return getRule().getDoNotApplyToClassNames();
+            }
+
+
+            public void setIgnoreRegex(String value) {
+                getRule().setIgnoreRegex(value);
+            }
+
+            public String getIgnoreRegex() {
+                return getRule().getIgnoreRegex();
+            }
+
+
+            public void setMaxNestedBlockDepth(int value) {
+                getRule().setMaxNestedBlockDepth(value);
+            }
+
+            public int getMaxNestedBlockDepth() {
+                return getRule().getMaxNestedBlockDepth();
+            }
 
     // custom code can be written after this line and it will be preserved during the regeneration
 
