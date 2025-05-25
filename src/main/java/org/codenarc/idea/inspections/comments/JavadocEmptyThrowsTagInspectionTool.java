eccd14d11e6f4ef7f6311bbb643196f5994abc5f
BercziSandor <BercziSandor@users.noreply.github.com>
2025-05-25 02:32:01 +0000

    Upgraded to CodeNarc 3.6.0-groovy-4.0

--- src/main/java/org/codenarc/idea/inspections/comments/JavadocEmptyThrowsTagInspectionTool.java
+++ src/main/java/org/codenarc/idea/inspections/comments/JavadocEmptyThrowsTagInspectionTool.java
@@ -1,58 +1,58 @@
 package org.codenarc.idea.inspections.comments;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.psi.PsiElement;
-import java.util.Collection;
-import java.util.Collections;
-import javax.annotation.Generated;
-import org.codenarc.idea.CodeNarcInspectionTool;
-import org.codenarc.rule.Violation;
-import org.codenarc.rule.comments.JavadocEmptyThrowsTagRule;
-import org.jetbrains.annotations.NotNull;
+        import com.intellij.codeInspection.LocalQuickFix;
+        import com.intellij.psi.PsiElement;
+        import java.util.Collection;
+        import java.util.Collections;
+        import javax.annotation.Generated;
+        import org.codenarc.idea.CodeNarcInspectionTool;
+        import org.codenarc.rule.Violation;
+        import org.codenarc.rule.comments.JavadocEmptyThrowsTagRule;
+        import org.jetbrains.annotations.NotNull;
 
-@Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
-public class JavadocEmptyThrowsTagInspectionTool extends CodeNarcInspectionTool<JavadocEmptyThrowsTagRule> {
+        @Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
+        public class JavadocEmptyThrowsTagInspectionTool extends CodeNarcInspectionTool<JavadocEmptyThrowsTagRule> {
 
-    // this code has been generated from org.codenarc.rule.comments.JavadocEmptyThrowsTagRule
+            // this code has been generated from org.codenarc.rule.comments.JavadocEmptyThrowsTagRule
 
-    public static final String GROUP = "Comments";
+            public static final String GROUP = "Comments";
 
-    public JavadocEmptyThrowsTagInspectionTool() {
-        super(new JavadocEmptyThrowsTagRule());
-        applyDefaultConfiguration(getRule());
-    }
+            public JavadocEmptyThrowsTagInspectionTool() {
+                super(new JavadocEmptyThrowsTagRule());
+                applyDefaultConfiguration(getRule());
+            }
 
-    @Override
-    public String getRuleset() {
-        return GROUP;
-    }
-
-
-    public void setAllowMultiline(boolean value) {
-        getRule().setAllowMultiline(value);
-    }
-
-    public boolean isAllowMultiline() {
-        return getRule().isAllowMultiline();
-    }
+            @Override
+            public String getRuleset() {
+                return GROUP;
+            }
 
 
-    public void setApplyToClassNames(String value) {
-        getRule().setApplyToClassNames(value);
-    }
+            public void setAllowMultiline(boolean value) {
+                getRule().setAllowMultiline(value);
+            }
 
-    public String getApplyToClassNames() {
-        return getRule().getApplyToClassNames();
-    }
+            public boolean isAllowMultiline() {
+                return getRule().isAllowMultiline();
+            }
 
 
-    public void setDoNotApplyToClassNames(String value) {
-        getRule().setDoNotApplyToClassNames(value);
-    }
+            public void setApplyToClassNames(String value) {
+                getRule().setApplyToClassNames(value);
+            }
 
-    public String getDoNotApplyToClassNames() {
-        return getRule().getDoNotApplyToClassNames();
-    }
+            public String getApplyToClassNames() {
+                return getRule().getApplyToClassNames();
+            }
+
+
+            public void setDoNotApplyToClassNames(String value) {
+                getRule().setDoNotApplyToClassNames(value);
+            }
+
+            public String getDoNotApplyToClassNames() {
+                return getRule().getDoNotApplyToClassNames();
+            }
 
     // custom code can be written after this line and it will be preserved during the regeneration
 
--- src/main/java/org/codenarc/idea/inspections/design/StatelessSingletonInspectionTool.java
+++ src/main/java/org/codenarc/idea/inspections/design/StatelessSingletonInspectionTool.java
@@ -1,58 +1,58 @@
 package org.codenarc.idea.inspections.design;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.psi.PsiElement;
-import java.util.Collection;
-import java.util.Collections;
-import javax.annotation.Generated;
-import org.codenarc.idea.CodeNarcInspectionTool;
-import org.codenarc.rule.Violation;
-import org.codenarc.rule.design.StatelessSingletonRule;
-import org.jetbrains.annotations.NotNull;
+        import com.intellij.codeInspection.LocalQuickFix;
+        import com.intellij.psi.PsiElement;
+        import java.util.Collection;
+        import java.util.Collections;
+        import javax.annotation.Generated;
+        import org.codenarc.idea.CodeNarcInspectionTool;
+        import org.codenarc.rule.Violation;
+        import org.codenarc.rule.design.StatelessSingletonRule;
+        import org.jetbrains.annotations.NotNull;
 
-@Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
-public class StatelessSingletonInspectionTool extends CodeNarcInspectionTool<StatelessSingletonRule> {
+        @Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
+        public class StatelessSingletonInspectionTool extends CodeNarcInspectionTool<StatelessSingletonRule> {
 
-    // this code has been generated from org.codenarc.rule.design.StatelessSingletonRule
+            // this code has been generated from org.codenarc.rule.design.StatelessSingletonRule
 
-    public static final String GROUP = "Design";
+            public static final String GROUP = "Design";
 
-    public StatelessSingletonInspectionTool() {
-        super(new StatelessSingletonRule());
-        applyDefaultConfiguration(getRule());
-    }
+            public StatelessSingletonInspectionTool() {
+                super(new StatelessSingletonRule());
+                applyDefaultConfiguration(getRule());
+            }
 
-    @Override
-    public String getRuleset() {
-        return GROUP;
-    }
-
-
-    public void setApplyToClassNames(String value) {
-        getRule().setApplyToClassNames(value);
-    }
-
-    public String getApplyToClassNames() {
-        return getRule().getApplyToClassNames();
-    }
+            @Override
+            public String getRuleset() {
+                return GROUP;
+            }
 
 
-    public void setDoNotApplyToClassNames(String value) {
-        getRule().setDoNotApplyToClassNames(value);
-    }
+            public void setApplyToClassNames(String value) {
+                getRule().setApplyToClassNames(value);
+            }
 
-    public String getDoNotApplyToClassNames() {
-        return getRule().getDoNotApplyToClassNames();
-    }
+            public String getApplyToClassNames() {
+                return getRule().getApplyToClassNames();
+            }
 
 
-    public void setInstanceRegex(String value) {
-        getRule().setInstanceRegex(value);
-    }
+            public void setDoNotApplyToClassNames(String value) {
+                getRule().setDoNotApplyToClassNames(value);
+            }
 
-    public String getInstanceRegex() {
-        return getRule().getInstanceRegex();
-    }
+            public String getDoNotApplyToClassNames() {
+                return getRule().getDoNotApplyToClassNames();
+            }
+
+
+            public void setInstanceRegex(String value) {
+                getRule().setInstanceRegex(value);
+            }
+
+            public String getInstanceRegex() {
+                return getRule().getInstanceRegex();
+            }
 
     // custom code can be written after this line and it will be preserved during the regeneration
 
