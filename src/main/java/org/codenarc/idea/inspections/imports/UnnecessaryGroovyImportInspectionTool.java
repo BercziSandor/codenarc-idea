d8743b867500d0a6075fff0b9ad0a0f1990a6ede
BercziSandor <BercziSandor@users.noreply.github.com>
2025-06-15 02:36:35 +0000

    Upgraded to CodeNarc 3.6.0-groovy-4.0

--- src/main/java/org/codenarc/idea/inspections/imports/UnnecessaryGroovyImportInspectionTool.java
+++ src/main/java/org/codenarc/idea/inspections/imports/UnnecessaryGroovyImportInspectionTool.java
@@ -1,31 +1,31 @@
 package org.codenarc.idea.inspections.imports;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.psi.PsiElement;
-import java.util.Collection;
-import java.util.Collections;
-import javax.annotation.Generated;
-import org.codenarc.idea.CodeNarcInspectionTool;
-import org.codenarc.rule.Violation;
-import org.codenarc.rule.imports.UnnecessaryGroovyImportRule;
-import org.jetbrains.annotations.NotNull;
+        import com.intellij.codeInspection.LocalQuickFix;
+        import com.intellij.psi.PsiElement;
+        import java.util.Collection;
+        import java.util.Collections;
+        import javax.annotation.Generated;
+        import org.codenarc.idea.CodeNarcInspectionTool;
+        import org.codenarc.rule.Violation;
+        import org.codenarc.rule.imports.UnnecessaryGroovyImportRule;
+        import org.jetbrains.annotations.NotNull;
 
-@Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
-public class UnnecessaryGroovyImportInspectionTool extends CodeNarcInspectionTool<UnnecessaryGroovyImportRule> {
+        @Generated("You can customize this class at the end of the file or remove this annotation to skip regeneration completely")
+        public class UnnecessaryGroovyImportInspectionTool extends CodeNarcInspectionTool<UnnecessaryGroovyImportRule> {
 
-    // this code has been generated from org.codenarc.rule.imports.UnnecessaryGroovyImportRule
+            // this code has been generated from org.codenarc.rule.imports.UnnecessaryGroovyImportRule
 
-    public static final String GROUP = "Imports";
+            public static final String GROUP = "Imports";
 
-    public UnnecessaryGroovyImportInspectionTool() {
-        super(new UnnecessaryGroovyImportRule());
-        applyDefaultConfiguration(getRule());
-    }
+            public UnnecessaryGroovyImportInspectionTool() {
+                super(new UnnecessaryGroovyImportRule());
+                applyDefaultConfiguration(getRule());
+            }
 
-    @Override
-    public String getRuleset() {
-        return GROUP;
-    }
+            @Override
+            public String getRuleset() {
+                return GROUP;
+            }
 
     // custom code can be written after this line and it will be preserved during the regeneration
 
